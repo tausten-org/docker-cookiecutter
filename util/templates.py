890da206edb63166de5e3300131a0eb3f1188725
Tyler Austen <TylerAusten@gmail.com>
2021-09-05 01:20:13 +0000

    feat: :sparkles: Method for parsing a template sequence from the template parameter in prep for cookiecutters support

---
+++ util/templates.py
@@ -0,0 +1,44 @@
+from dataclasses import dataclass
+from typing import AnyStr
+
+"""Gather complete set of information about the template source.
+
+template - the base uri of the template (eg. directory, or repository address)
+checkout - [optional] - branch, tag or commit within the repo
+directory - [optional] - directory within the repo 
+"""
+@dataclass
+class TemplateSourceInfo:
+    template: AnyStr
+    checkout: AnyStr = None
+    directory: AnyStr = None
+
+# Work around python's lack of consts
+class CONST(object):
+    __slots__ = ()
+    TEMPLATE_DELIM = ",,"
+    COMPONENT_DELIM = ","
+    CHECKOUT_PREFIX = "checkout="
+    DIRECTORY_PREFIX = "directory="
+CONST = CONST()
+
+def parse_template_sources(template):
+    result = []
+    template_strings = template.split(CONST.TEMPLATE_DELIM)
+
+    for t in template_strings:
+        template = None
+        checkout = None
+        directory = None
+
+        for c in t.split(CONST.COMPONENT_DELIM):
+            if c.startswith(CONST.CHECKOUT_PREFIX):
+                checkout = c[len(CONST.CHECKOUT_PREFIX):]
+            elif c.startswith(CONST.DIRECTORY_PREFIX):
+                directory = c[len(CONST.DIRECTORY_PREFIX):]
+            else:
+                template = c
+
+        result.append(TemplateSourceInfo(template, checkout, directory))
+
+    return result
