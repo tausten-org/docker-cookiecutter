6bc96e2651210d8baaf5765de476756bb7881d18
Tyler Austen <TylerAusten@gmail.com>
2021-09-11 23:40:47 +0000

    refactor: :art: refactor: :art: refactor: :art: refactor: :art: Flake8 suggestions / refactorings.

--- docker_cookiecutter/templates.py
+++ docker_cookiecutter/templates.py
@@ -1,17 +1,21 @@
 from dataclasses import dataclass
 from typing import AnyStr
 
+
 """Gather complete set of information about the template source.
 
 template - the base uri of the template (eg. directory, or repository address)
 checkout - [optional] - branch, tag or commit within the repo
-directory - [optional] - directory within the repo 
+directory - [optional] - directory within the repo
 """
+
+
 @dataclass
 class TemplateSourceInfo:
     template: AnyStr
     checkout: AnyStr = None
     directory: AnyStr = None
+
 
 # Work around python's lack of consts
 class CONST(object):
@@ -20,16 +24,22 @@ class CONST(object):
     COMPONENT_DELIM = ","
     CHECKOUT_PREFIX = "checkout="
     DIRECTORY_PREFIX = "directory="
+
+
 CONST = CONST()
+
 
 def encode_template_sources(sources):
     templates = []
     for t in sources:
         template = t.template
-        if t.checkout: template += CONST.COMPONENT_DELIM + CONST.CHECKOUT_PREFIX + t.checkout
-        if t.directory: template += CONST.COMPONENT_DELIM + CONST.DIRECTORY_PREFIX + t.directory
+        if t.checkout:
+            template += CONST.COMPONENT_DELIM + CONST.CHECKOUT_PREFIX + t.checkout
+        if t.directory:
+            template += CONST.COMPONENT_DELIM + CONST.DIRECTORY_PREFIX + t.directory
         templates.append(template)
     return CONST.TEMPLATE_DELIM.join(templates)
+
 
 def decode_template_sources(template):
     result = []
@@ -43,9 +53,9 @@ def decode_template_sources(template):
 
             for c in t.split(CONST.COMPONENT_DELIM):
                 if c.startswith(CONST.CHECKOUT_PREFIX):
-                    checkout = c[len(CONST.CHECKOUT_PREFIX):]
+                    checkout = c[len(CONST.CHECKOUT_PREFIX) :]
                 elif c.startswith(CONST.DIRECTORY_PREFIX):
-                    directory = c[len(CONST.DIRECTORY_PREFIX):]
+                    directory = c[len(CONST.DIRECTORY_PREFIX) :]
                 else:
                     template = c
 
