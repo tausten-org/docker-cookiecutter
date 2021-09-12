a11d392f66482dd27ca7278f999df028e564661a
Tyler Austen <TylerAusten@gmail.com>
2021-09-12 00:18:05 +0000

    feat: :sparkles: Hook up cli.py to the args thing directly for now. We'll switch to supporting the multiple commands in a bit.

--- docker_cookiecutter/cli.py
+++ docker_cookiecutter/cli.py
@@ -1,9 +1,12 @@
 """Console script for docker_cookiecutter."""
 
+import sys
+
+from docker_cookiecutter.suggest import cookiecutter_to_docker_args
+
 
 def main():
-    # TODO: Do stuff
-    print("HELLO WORLD!")
+    print(" ".join(cookiecutter_to_docker_args(sys.argv[1:])))
 
 
 if __name__ == "__main__":
